dd62cf44300328db8918640e257309892ff6fcb4
Bob Anderson <bobanderson.malibu@gmail.com>
2023-05-20 18:38:20 -0700

    updated to udemy repo

--- main.go
+++ main.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
+	"github.com/bobanderson-malibu/bookings-app/pkg/config"
 	"github.com/bobanderson-malibu/bookings-app/pkg/handlers"
+	"github.com/bobanderson-malibu/bookings-app/pkg/render"
 )
 
-var portNumber = ":8080"
+const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main function
 func main() {
-	// fmt.Println("Hello Gopher!")
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	// change this to true when in production
+	app.InProduction = false
 
-	_ = http.ListenAndServe(portNumber, nil)
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
